aecd14e408f8310bf16e06f33fb1a54188740b16
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-17 16:06:55 +0200

    tunnikontroll2

---
+++ Tunnikontroll_/6versioon/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6versioon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST.");
+            Console.WriteLine("-------------------------------------------");
+
+            double total = 0;
+
+            while (true)
+            {
+                Console.Write("Enter price of food item [-1 to quit]: ");
+                string input = Console.ReadLine();
+
+                
+                double value = double.Parse(input);
+
+               
+
+                if (value == -1)
+                    break;
+
+                total = total + value;
+
+            }
+
+            double gratuity = total * 0.15;
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine($"Subtotal: €{total}");   
+            Console.WriteLine($"15.00 % Gratuity: €{total * 0.15}");
+            Console.WriteLine($"Total: €{total - gratuity}");
+
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tunnikontroll_/Tunnikontroll_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnikontroll_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST.");
+            Console.WriteLine("-------------------------------------------");
+
+            int input = 0;
+            while (input != -1)
+            {
+                Console.Write("Enter price of food item [-1 to quit]: ");
+                string input = Console.ReadLine();
+            }
+
+          /* else (input == -1)
+                {
+
+            }
+                
+      */
+                       
+           
+            /*int sisestatudHinnad = int.Parse(input);
+            
+            if (input == -1)
+            {
+Console.Write("Enter price of food item [-1 to quit]: ");
+            }
+            //Console.WriteLine(input);
+            */
+
+           // Console.ReadLine();
+        /*Console.WriteLine("-------------------------------------------");
+         Console.Write("Subtotal: );
+         Console.Write("15.00 % Gratuity: €");
+         Console.Write("Total: €");
+         */
+        }
+    }
+}

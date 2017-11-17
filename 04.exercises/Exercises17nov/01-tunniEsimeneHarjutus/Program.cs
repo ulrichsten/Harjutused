976ea72678987f0a3b987c52de342e9421b399b6
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-17 14:09:14 +0200

    lisasin tühiku

--- 04.exercises/Exercises17nov/01-tunniEsimeneHarjutus/Program.cs
+++ 04.exercises/Exercises17nov/01-tunniEsimeneHarjutus/Program.cs
@@ -14,7 +14,7 @@ namespace _01_tunniEsimeneHarjutus
             Console.WriteLine("Tere, kirjuta oma nimi");
             Console.WriteLine();
 
-            Console.Write("Sinu nimi: ");
+            Console.Write("Sinu nimi:  ");
             string sinuNimi = Console.ReadLine();
 
             Console.WriteLine(sinuNimi);

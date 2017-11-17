53839111dfbc5fafff78db5029de808721deb525
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-17 13:53:44 +0200

    created new solution

---
+++ 04.exercises/Exercises17nov/01-tunniEsimeneHarjutus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_tunniEsimeneHarjutus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tere, kirjuta oma nimi");
+            Console.Write("Sinu nimi: ");
+            string sinuNimi = Console.ReadLine();
+
+            Console.WriteLine(sinuNimi);
+            Console.ReadLine();
+            
+        }
+    }
+}
